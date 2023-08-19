70f3c57f0c5f8ab375184ea8522e6250461b917e
Jörg Walter <jwdev@posteo.de>
2023-08-19 14:03:19 +0200

    generalized run function

--- internal/program/create.go
+++ internal/program/create.go
@@ -6,29 +6,7 @@ import (
 	"github.com/jwdev42/xtagger/internal/cli"
 	"github.com/jwdev42/xtagger/internal/io/filesystem"
 	"github.com/jwdev42/xtagger/internal/record"
-	"io/fs"
-	"os"
 )
-
-func runCreate(cmdline *cli.CommandLine) error {
-	for _, path := range cmdline.Paths() {
-		info, err := os.Lstat(path)
-		if err != nil {
-			return err
-		}
-		if info.Mode()&fs.ModeSymlink == fs.ModeSymlink {
-
-		} else if info.Mode()&fs.ModeDir == fs.ModeDir {
-			err = tagDir(cmdline, path)
-		} else {
-			err = tagFile(cmdline, path)
-		}
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
 
 func tagDir(cmdline *cli.CommandLine, path string) error {
 	ctx, cancel := context.WithCancel(context.Background())
--- internal/program/program.go
+++ internal/program/program.go
@@ -3,6 +3,8 @@ package program
 import (
 	"fmt"
 	"github.com/jwdev42/xtagger/internal/cli"
+	"io/fs"
+	"os"
 )
 
 func Run() error {
@@ -13,9 +15,32 @@ func Run() error {
 	}
 	switch command := cmdline.Command(); command {
 	case cli.CommandCreate:
-		return runCreate(cmdline)
+		return run(cmdline, tagDir, tagFile)
 	default:
 		return fmt.Errorf("Unknown command \"%s\"", command)
+	}
+	return nil
+}
+
+func run(cmdline *cli.CommandLine, dirFunc, fileFunc func(*cli.CommandLine, string) error) error {
+	for _, path := range cmdline.Paths() {
+		info, err := os.Lstat(path)
+		if err != nil {
+			return err
+		}
+		if info.Mode()&fs.ModeSymlink == fs.ModeSymlink {
+			if !cmdline.FlagFollowSymlinks() {
+				continue
+			}
+			err = dirFunc(cmdline, path)
+		} else if info.Mode()&fs.ModeDir == fs.ModeDir {
+			err = dirFunc(cmdline, path)
+		} else {
+			err = fileFunc(cmdline, path)
+		}
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
