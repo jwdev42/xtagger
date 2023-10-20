2299d2e2196a4ede94616c4024ff2a7a6832fb0e
Jörg Walter <jwdev@posteo.de>
2023-10-20 16:18:45 +0200

    Types record.Record and record.Attribute now have their own separate files.

---
+++ internal/record/attr.go
@@ -0,0 +1,77 @@
+package record
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/xattr"
+)
+
+// Represents the whole content of a user.xtagger xattr entry
+type Attribute map[string]*Record
+
+// Loads the xtagger extended attribute for path.
+func LoadAttribute(path string) (Attribute, error) {
+	//Read extended attribute
+	payload, err := xattr.Get(path, attrName)
+	if err != nil {
+		//Wrap error to be able to catch ENOATTR
+		return nil, fmt.Errorf("Failed to read extended attribute: %w", err)
+	}
+	//Decode JSON
+	attr := make(Attribute)
+	if err := json.Unmarshal([]byte(payload), &attr); err != nil {
+		return nil, fmt.Errorf("Failed to decode json: %s", err)
+	}
+	//Validation
+	if err := attr.validate(); err != nil {
+		return nil, err
+	}
+	return attr, nil
+}
+
+// Stores the xtagger extended attribute in path's inode.
+func (r Attribute) Store(path string) error {
+	if r == nil {
+		panic("BUG: Calling Store() with a nil receiver is prohibited")
+	}
+	//Validation
+	if err := r.validate(); err != nil {
+		return err
+	}
+	//Encode JSON
+	payload, err := json.Marshal(r)
+	if err != nil {
+		return fmt.Errorf("Failed to encode json: %s", err)
+	}
+	//Write extended attribute
+	if err := xattr.Set(path, attrName, payload); err != nil {
+		return fmt.Errorf("Failed to write extended attribute: %s", err)
+	}
+	return nil
+}
+
+// Returns the newest Record. Returns zero-values if no record was found.
+func (r Attribute) MostRecent() (name string, rec *Record) {
+	if r == nil || len(r) < 1 {
+		return "", nil
+	}
+	for k, v := range r {
+		if rec == nil || v.Timestamp > rec.Timestamp {
+			name = k
+			rec = v
+		}
+	}
+	return name, rec
+}
+
+func (r Attribute) validate() error {
+	if r == nil {
+		return fmt.Errorf("Attribute %s cannot be null", attrName)
+	}
+	for name, rec := range r {
+		if err := rec.validate(); err != nil {
+			return fmt.Errorf("Validation for record \"%s\" failed: %s", name, err)
+		}
+	}
+	return nil
+}
--- internal/record/record.go
+++ internal/record/record.go
@@ -1,11 +1,9 @@
 package record
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/jwdev42/xtagger/internal/hashes"
-	"github.com/pkg/xattr"
 	"time"
 )
 
@@ -58,76 +56,6 @@ func (r *Record) validate() error {
 	for i, ch := range []rune(r.Checksum) {
 		if !(ch >= 48 && ch <= 57 || ch >= 97 && ch <= 102) {
 			return fmt.Errorf("Checksum has illegal character at index %d", i)
-		}
-	}
-	return nil
-}
-
-// Represents the whole content of a user.xtagger xattr entry
-type Attribute map[string]*Record
-
-// Loads the xtagger extended attribute for path.
-func LoadAttribute(path string) (Attribute, error) {
-	//Read extended attribute
-	payload, err := xattr.Get(path, attrName)
-	if err != nil {
-		//Wrap error to be able to catch ENOATTR
-		return nil, fmt.Errorf("Failed to read extended attribute: %w", err)
-	}
-	//Decode JSON
-	attr := make(Attribute)
-	if err := json.Unmarshal([]byte(payload), &attr); err != nil {
-		return nil, fmt.Errorf("Failed to decode json: %s", err)
-	}
-	//Validation
-	if err := attr.validate(); err != nil {
-		return nil, err
-	}
-	return attr, nil
-}
-
-// Stores the xtagger extended attribute in path's inode.
-func (r Attribute) Store(path string) error {
-	if r == nil {
-		panic("BUG: Calling Store() with a nil receiver is prohibited")
-	}
-	//Validation
-	if err := r.validate(); err != nil {
-		return err
-	}
-	//Encode JSON
-	payload, err := json.Marshal(r)
-	if err != nil {
-		return fmt.Errorf("Failed to encode json: %s", err)
-	}
-	//Write extended attribute
-	if err := xattr.Set(path, attrName, payload); err != nil {
-		return fmt.Errorf("Failed to write extended attribute: %s", err)
-	}
-	return nil
-}
-
-// Returns the newest Record. Returns zero-values if no record was found.
-func (r Attribute) MostRecent() (name string, rec *Record) {
-	if r == nil || len(r) < 1 {
-		return "", nil
-	}
-	for k, v := range r {
-		if rec == nil || v.Timestamp > rec.Timestamp {
-			name = k
-			rec = v
-		}
-	}
-	return name, rec
-}
-
-func (r Attribute) validate() error {
-	if r == nil {
-		return fmt.Errorf("Attribute %s cannot be null", attrName)
-	}
-	for name, rec := range r {
-		if err := rec.validate(); err != nil {
-			return fmt.Errorf("Validation for record \"%s\" failed: %s", name, err)
 		}
 	}
 	return nil
